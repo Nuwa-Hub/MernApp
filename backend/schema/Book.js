9902e3a29eca10ffe75b195a5d4fa8fa1168b272
nuwan <nuwandeshapriyaaa@gmail.com>
2025-02-22 15:44:53 +0530

    initial comit

---
+++ backend/schema/Book.js
@@ -0,0 +1,12 @@
+// Create a model
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+    title: String,
+    author: String,
+    year: String
+});
+
+const Book = mongoose.model('Book', bookSchema);
+
+exports.Book = Book;
